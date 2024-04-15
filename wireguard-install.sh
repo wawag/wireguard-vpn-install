b5749c5b7297b24a8b01123fe145c58f24061657
wawag <16876759+wawag@users.noreply.github.com>
2024-04-15 20:12:31 +0800

    Not install qrencode

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -173,7 +173,8 @@ function installWireGuard() {
 	# Install WireGuard tools and module
 	if [[ ${OS} == 'ubuntu' ]] || [[ ${OS} == 'debian' && ${VERSION_ID} -gt 10 ]]; then
 		apt-get update
-		apt-get install -y wireguard iptables resolvconf qrencode
+		# apt-get install -y wireguard iptables resolvconf qrencode
+                apt-get install -y wireguard iptables resolvconf
 	elif [[ ${OS} == 'debian' ]]; then
 		if ! grep -rqs "^deb .* buster-backports" /etc/apt/; then
 			echo "deb http://deb.debian.org/debian buster-backports main" >/etc/apt/sources.list.d/backports.list
